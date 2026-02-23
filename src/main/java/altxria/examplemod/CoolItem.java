582b060f757310728f4f31cada8c94c79c18f592
altxria <altxria@pm.me>
2026-02-23 00:43:43 -0600

    got started

---
+++ src/main/java/altxria/examplemod/CoolItem.java
@@ -0,0 +1,8 @@
+package altxria.examplemod;
+import net.minecraft.core.item.Item;
+
+public class CoolItem extends Item {
+	public CoolItem(String translationKey, String namespaceId, int itemId) {
+		super(translationKey, namespaceId, itemId);
+	}
+}
--- src/main/java/altxria/examplemod/ExampleMod.java
+++ src/main/java/altxria/examplemod/ExampleMod.java
@@ -1,28 +1,51 @@
-package turniplabs.examplemod;
+package altxria.examplemod;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.core.item.Item;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import turniplabs.halplibe.helper.ItemBuilder;
 import turniplabs.halplibe.util.GameStartEntrypoint;
 import turniplabs.halplibe.util.RecipeEntrypoint;
 
 public class ExampleMod implements ModInitializer, RecipeEntrypoint, GameStartEntrypoint {
+
 	public static final String MOD_ID = "examplemod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	// item work
+	ItemBuilder GenericItemBuilder = new ItemBuilder(ExampleMod.MOD_ID);
+	public static Item CoolItem;
+	private static int currentItemID = 30000;
+
+	private static int newItemID() {
+		return currentItemID++;
+	}
+	// end item work
+
 	@Override
 	public void onInitialize() {
 		LOGGER.info("ExampleMod initialized.");
 	}
 
 	@Override
-	public void onRecipesReady() {}
+	public void onRecipesReady()
+	{
+
+	}
 
 	@Override
 	public void initNamespaces() {}
 
 	@Override
-	public void beforeGameStart() {}
+	public void beforeGameStart() {
+
+	}
 
 	@Override
-	public void afterGameStart() {}
+	public void afterGameStart() {
+		CoolItem = GenericItemBuilder
+			.build(new CoolItem("CoolItem", ExampleMod.MOD_ID + ":CoolItem", newItemID()));
+	}
 }
+
